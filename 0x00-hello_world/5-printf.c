beceb044a5dd8ecf6f70961fcad83117dfec40ca
Hananiah Henry <hananiahjoseph66@gmail.com>
2023-02-16 13:13:21 -0800

    1

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
- * main - entry point 
- * return;
- */
+*main - A program that prints a line using the printf function
+*return:0 (success)
+*/
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
