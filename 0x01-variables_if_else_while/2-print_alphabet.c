500b46b032be89205891bfc008aeadd24c927d71
Hananiah Henry <hananiahjoseph66@gmail.com>
2023-02-18 06:37:05 -0800

    1

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * 
+ * Description - program that prints the alphabet in lowercase
+ *
+ * Return : Always 0 (success)
+ */
+int main(void)
+{
+	char[26] = 'a
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description - prints all single digit numbers of base
+ *
+ * Return : Always 0(success)
+ */
+int main(void)
+{
+	int n = 0;
+
+	for (n = 0; n < 10; n++)
+		printf("%d \n", n);
+	return (0);
+}
