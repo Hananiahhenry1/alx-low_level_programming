8a4d91322551e5db5436f2c1860253cd87dece55
Hananiah Henry <hananiahjoseph66@gmail.com>
2023-02-19 10:19:19 -0800

    1

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,12 +9,13 @@ int main(void)
 	int a = 0;
 
 	for (a = 0; a < 10; a++)
-		putchar(a + '0');
-	if (a < 9)
 	{
-		putchar(',');
-		putchar(' ');
-	}
+		putchar(a + '0');
+		if (a < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}`
 	putchar('\n');
 	return (0);
 }
