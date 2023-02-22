dd78a5309d21de321a5757dd5213e3c024f94af4
hananiah henry <hananiahjoseph66@gmail.com>
2023-02-22 20:47:30 +0000

    1

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_to_98 - entry point
+ * Description - prints all natural numbers from n to 98
+ * Return: success
+ */
+void print_to_98(int n)
+{
+	int nat;
+	nat = n;
+	int i, j;
+
+	if (nat <= 98)
+	{
+		for (i = 0; i <= num; i++)
+		{
+
