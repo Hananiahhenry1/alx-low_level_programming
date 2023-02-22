0bf94eaca5155898c1f70ff9993fd3a2cfb32a36
hananiah henry <hananiahjoseph66@gmail.com>
2023-02-22 21:24:50 +0000

    1

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,13 +7,13 @@ int main(void)
 {
 	int i, n = 50;
 	int t1 = 1, t2 = 2;
-	int nextTerm = t1 + t2;
+	long long nextTerm = t1 + t2;
 
 	for (i = 3; i <= n; ++i)
 	{
-		printf("%d, ", nextTerm);
+		printf("%lld, ", nextTerm);
 		t1 = t2;
-		t2 = nextTerm = t1 + t2;
+		t2 = nextTerm;
 	}
 	printf("\n");
 	return (0);
