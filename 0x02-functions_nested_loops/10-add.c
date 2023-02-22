0c698ad4ffd0b6ad2fcab3696250781eb129090c
Hananiah Henry <hananiahjoseph66@gmail.com>
2023-02-22 02:40:20 -0800

    1

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,12 +1,15 @@
 #include "main.h"
 /**
- * main - Entry point
- * Return: 0 always
+ * add - a and b
+ * Description - that adds two integers (a and b) returns the result
+ * a: interger 1
+ * b: interger 2
+ * Return: sum
  */
 int add(int a, int b)
 {
 	int sum;
 
 	sum = a + b;
-	return sum;
+	return (sum);
 }
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet
+ * Return: print_alphabet_x10
+ */
+void print_alphabet_x10(void)
+{
+	int x10;
+	char alp = 'a';
+
+	for (x10 = 0; x10 <= 10; x10++)
+	{
+		for (alp = 'a'; alp <= 'z'; alp++)
+			_putchar(alp);
+		_putchar('\n');
+	}
+}
