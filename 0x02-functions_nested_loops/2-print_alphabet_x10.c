71cc4c09257f14be0d0570b61bfa04f0fd23717a
Hananiah Henry <hananiahjoseph66@gmail.com>
2023-02-22 02:44:25 -0800

    1

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,7 +8,7 @@ void print_alphabet_x10(void)
 	int x10;
 	char alp = 'a';
 
-	for (x10 = 0; x10 <= 10; x10++)
+	for (x10 = 0; x10 < 10; x10++)
 	{
 		for (alp = 'a'; alp <= 'z'; alp++)
 			_putchar(alp);
