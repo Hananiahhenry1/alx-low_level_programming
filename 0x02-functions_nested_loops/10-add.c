e58af4e503bba1b907bb9c06ab8ef2ce97e7a1bb
Hananiah Henry <hananiahjoseph66@gmail.com>
2023-02-22 02:49:46 -0800

    1

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,8 +2,8 @@
 /**
  * add - a and b
  * Description - that adds two integers (a and b) returns the result
- * a: interger 1
- * b: interger 2
+ * @a: interger 1
+ * @b: interger 2
  * Return: sum
  */
 int add(int a, int b)
