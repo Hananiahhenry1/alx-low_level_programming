1125d26833513c684fa2ead1e5fae1796deab3ec
Hananiah Henry <hananiahjoseph66@gmail.com>
2023-02-23 14:35:05 -0800

    1

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,18 +5,12 @@
  */
 void print_most_numbers(void)
 {
-	int a;
+	char a;
 
-	for (a = 0; a < 10; a++)
+	for (a = '0'; a < '9'; a++)
 	{
-		if (a == 2 || a == 4)
-		{
-			continue;
-		}
-		else
-		{
-			_putchar(a + '0');
+		if (!(a == 2 || a == 4))
+			_putchar(a);
 		_putchar('\n');
-		}
 	}
 }
