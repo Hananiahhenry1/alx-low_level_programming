079a50afc69d4636c839a9ed372d35f66a69fd01
Hananiah Henry <hananiahjoseph66@gmail.com>
2023-02-23 13:58:04 -0800

    1

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * more_numbers - Entry point
+ * Return: success
+ */
+void more_numbers(void)
+{
+	int a, b;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b < 14; b++)
+		{
+			if (b > 9)
+				_putchar((b / 10) + '0');
+			_putchar((b % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}				
