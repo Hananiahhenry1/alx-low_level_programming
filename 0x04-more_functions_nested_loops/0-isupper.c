48b898515ab95cd6d972095fec3a6ebd3c14a55a
Hananiah Henry <hananiahjoseph66@gmail.com>
2023-02-23 13:01:50 -0800

    1

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isupper - Entry point
+ * @c: value to compare
+ * Return: success
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
