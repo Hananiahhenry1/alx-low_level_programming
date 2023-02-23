652899d9ef81cb78fbd74a653ec6b24aabc8b922
Hananiah Henry <hananiahjoseph66@gmail.com>
2023-02-23 13:17:42 -0800

    1

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - Entry point
+ * @a: value 
+ * @b: value
+ * Return: 1 or 2
+ */
+int mul(int a, int b)
+{
+	int product;
+
+	product = a * b;
+	return (product);
+}
