afa99c29c243ebd68c933c39c8fdbad5054b7d62
Hananiah Henry <hananiahjoseph66@gmail.com>
2023-02-23 13:38:06 -0800

    1

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_numbers - Entry point
+ * Return: success
+ */
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 0; a <= 10; a++)
+		_putchar(a + '0');
+	_putchar('\n');
+}
