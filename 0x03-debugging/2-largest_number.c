3e81a0819430a9c5dc3766dc7aad750e8a8eb184
Hananiah Henry <hananiahjoseph66@gmail.com>
2023-02-23 11:57:12 -0800

    1

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,24 +10,23 @@
 
 int largest_number(int a, int b, int c)
 {
-    int largest;
+	int largest;
 
-    if (a > b && a > c)
-    {
-        largest = a;
-    }
-    else if (b > c && b > a)
-    {
-        largest = b;
-    }
-    else if (c > b)
-    {
-	    largest = c;
-    }
-    else
-    {
-        largest = b;
-    }
-
-    return (largest);
+	if (a > b && a > c)
+	{
+		largest = a;
+	}
+	else if (b > c && b > a)
+	{
+		largest = b;
+	}
+	else if (c > b)
+	{
+		largest = c;
+	}
+	else
+	{
+		largest = b;
+	}
+	return (largest);
 }
