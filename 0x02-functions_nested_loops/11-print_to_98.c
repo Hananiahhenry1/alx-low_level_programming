c3357bbce28e40b45a1916976a1e9233d06f87cb
Hananiah Henry <hananiahjoseph66@gmail.com>
2023-02-26 23:55:28 -0800

    og 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,12 +12,17 @@ void print_to_98(int n)
 	if (n <= 98)
 	{
 		for (i = n; i <= 98; i++)
-			printf("%d, ", i);
+		{
+			printf("%d", i);
+			printf("%d " 98);
 	}
 	else
 	{
 		for (i = n; i >= 98; i--)
-			printf("%d, ", i);
+		{
+			printf("%d", i);
+			printf("%d", 98);
+		}
 	}
 	printf("\n");
 }
