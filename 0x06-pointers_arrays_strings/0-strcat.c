43fb20cbd7a02a407c315f89727248e307d144c2
Hananiah Henry <hananiahjoseph66@gmail.com>
2023-03-02 10:19:01 -0800

    trial

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strcat - entry point
+ * @dest: string 1
+ * @src: string 2
+ * Return: dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	char str1 = dest;
+	char str2 = src;
+	char total;
+
+	total = strcat(str1, atr2);
+
+	return total;
+}
