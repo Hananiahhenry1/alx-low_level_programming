532b28b7397d57ba42f2f0b7d4e931cf393f49db
Hananiah Henry <hananiahjoseph66@gmail.com>
2023-03-06 14:23:43 -0800

    fix

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -8,9 +8,12 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
+	unsigned int i = 0;
 
 	while (i < n)
+	{
 		s[i] = b;
+		i++;
+	}
 	return (s);
 }
--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,7 +8,7 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i;
+	unsigned int i = 0;
 
 	while (i < n)
 	{
