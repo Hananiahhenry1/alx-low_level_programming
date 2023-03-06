7c19d6e1e67ae1a3406ca711cb5217538e8e52bc
Hananiah Henry <hananiahjoseph66@gmail.com>
2023-03-06 14:16:32 -0800

    memory and arrays

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _memset - entry point
+ * @s: pointer of memory area
+ * @b: constant byte
+ * @n: bytes of memory to be pointed by s
+ * Return: memory area
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	while (i < n)
+		s[i] = b;
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _memcpy - copies memory area
+ * @dest: destionation
+ * @src: source
+ * @int: size
+ * Return: destionation to copy to
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	while (i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
