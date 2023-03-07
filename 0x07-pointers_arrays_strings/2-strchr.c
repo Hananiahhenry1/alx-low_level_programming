c79c8559467411a5156638186c9e68b3670aff8a
Hananiah Henry <hananiahjoseph66@gmail.com>
2023-03-07 02:11:07 -0800

    recursion

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+/**
+ * _strchr - locates a character in a string
+ * @s: pointer string
+ * @c: string
+ * Return: c is string found or NULL is non
+ */
+char *_strchr(char *s, char c)
+{
+	char i;
+
+	for (i = 0; i <= s[i]; i++)
+	{
+		if (c == s[i])
+			return (c);
+		else
+			return (0);
+	}
+}
