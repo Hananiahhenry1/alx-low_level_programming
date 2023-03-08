216561324c8d5e05e21fd5b9dd0604f4d71f193a
Hananiah Henry <hananiahjoseph66@gmail.com>
2023-03-08 07:32:19 -0800

    test

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _puts_recursion - prints a string, followed by a new line
+ * @s: string pointer
+ */
+void _puts_recursion(char *s)
+{
+	int i = 0;
+
+	while (s[i] == '\0')
+		s[i]++;
+}
