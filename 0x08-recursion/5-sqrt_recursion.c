32dadc967efcea46018cd348270713c6a5e20446
Hananiah Henry <hananiahjoseph66@gmail.com>
2023-03-09 10:31:13 -0800

    new

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - square of a number
+ * @n: value
+ * Return: -1 or value
+ */
+int _sqrt_recursion(int n)
+{
+	return (is_sqrt(n, 1));
+}
+is_sqrt(int n, int i)
+{
+	if (i * i < n)
+		return (-1);
+	else if (i * i > n)
+		{
+			++i;
+			return (is_sqrt(n, i));
+		}
+	else
+		return (i);
+}
