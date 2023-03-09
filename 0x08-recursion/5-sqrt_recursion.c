e119b73b73594cd3179f91974b85f0b507814712
Hananiah Henry <122923660+Hananiahhenry1@users.noreply.github.com>
2023-03-09 19:53:28 +0100

    Update 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,11 +3,7 @@
  * _sqrt_recursion - square of a number
  * @n: value
  * Return: -1 or value
- * is_sqrt - for the recursion
- * @i: test the hyphothesis
  */
-int is_sqrt(int n, int i);
-
 int _sqrt_recursion(int n)
 {
 	return (is_sqrt(n, 1));
@@ -15,6 +11,7 @@ int _sqrt_recursion(int n)
 /**
  * is_sqrt - for the recursion
  * @i: test for the hypthothesis
+ * @n: value
  */
 int is_sqrt(int n, int i)
 {
