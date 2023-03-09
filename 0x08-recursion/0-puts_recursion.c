3a113089777d146419cfc948f70bcb0112117b94
Hananiah Henry <hananiahjoseph66@gmail.com>
2023-03-08 20:28:29 -0800

    new

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,8 +5,13 @@
  */
 void _puts_recursion(char *s)
 {
-	int i = 0;
-
-	while (s[i] == '\0')
-		s[i]++;
+	if (*s == '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else 
+	{
+		_putchar('\n');
+	}
 }
