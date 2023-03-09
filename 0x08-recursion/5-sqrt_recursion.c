637af88be0029a2ca22b15285f7c50033671e1e8
Hananiah Henry <hananiahjoseph66@gmail.com>
2023-03-09 10:43:42 -0800

    new

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,16 +3,18 @@
  * _sqrt_recursion - square of a number
  * @n: value
  * Return: -1 or value
+ * is_sqrt - for the recursion
+ * @i: test the hyphothesis
  */
 int _sqrt_recursion(int n)
 {
 	return (is_sqrt(n, 1));
 }
-is_sqrt(int n, int i)
+int is_sqrt(int n, int i)
 {
 	if (i * i > n)
 		return (-1);
-	else if (i * i  n)
+	else if (i * i < n)
 		{
 			++i;
 			return (is_sqrt(n, i));
