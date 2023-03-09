96131a899c2bd62d8ab975a3cff8dea14514d429
Hananiah Henry <hananiahjoseph66@gmail.com>
2023-03-09 04:08:44 -0800

    new

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _pow_recursion - entry
+ * @x: number
+ * @y: raise to its power
+ * Return: -1 or result
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else
+		return (x ** y);
+}
