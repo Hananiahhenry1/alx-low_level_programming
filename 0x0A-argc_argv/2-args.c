b2c6bcaa32306bcca9ef72f3012093e59498e5c8
Hananiah Henry <hananiahjoseph66@gmail.com>
2023-03-10 04:31:11 -0800

    new

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main - entry point
+ * @argv: argument value
+ * @argc: argument count
+ * Return: success
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	int i = 0;
+
+	while(i < argc)
+	{
+		printf("%s\n",  argv[i]);
+		i++;
+	}
+	return (0);
+}
