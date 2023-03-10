12e467b9d60383a54c97a1c224147ed32a301e6d
Hananiah Henry <122923660+Hananiahhenry1@users.noreply.github.com>
2023-03-10 21:02:49 +0100

    Create 2-strchr.c

---
+++ 0x09-static_libraries/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strchr - locates a character in a string
+ * @s: pointer string
+ * @c: string
+ * Return: c is string found or NULL is non
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return ((char *)s);
+		s++;
+	}
+	if (*s == c)
+		return ((char *)s);
+	return ('\0');
+}
