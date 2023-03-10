691576eb07704b6db8c737a8772727618f637b70
Hananiah Henry <122923660+Hananiahhenry1@users.noreply.github.com>
2023-03-10 21:14:31 +0100

    Create 6-abs.c

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - Entry point
+ * @n: computes the absolute value of an integer.
+ * Return: success
+ */
+int _abs(int n)
+{
+	if (n > 0)
+		return (n);
+	else
+		return (-n);
+}
