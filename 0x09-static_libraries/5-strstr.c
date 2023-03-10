1647ffd939de79dbbf9081c712fff0cccb14307f
Hananiah Henry <122923660+Hananiahhenry1@users.noreply.github.com>
2023-03-10 21:13:01 +0100

    Create 5-strstr.c

---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strstr - entry point
+ * @haystack: input
+ * @needle: input
+ * Return: Always 0
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		while (*haystack == *needle && *needle != '\0')
+		{
+			haystack++;
+			needle++;
+		}
+		if (*needle == '\0')
+			return (haystack);
+	}
+	return ('\0');
+}
