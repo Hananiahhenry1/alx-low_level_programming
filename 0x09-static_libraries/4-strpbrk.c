3c10c8376f27b3b71ebed5b2782d0ca86298ff85
Hananiah Henry <122923660+Hananiahhenry1@users.noreply.github.com>
2023-03-10 22:46:00 +0100

    Update 4-strpbrk.c

--- 0x09-static_libraries/4-strpbrk.c
+++ 0x09-static_libraries/4-strpbrk.c
@@ -1,22 +1,23 @@
 #include "main.h"
+
 /**
- * _strpbrk - string in any set of bytes
- * @s: pointer
- * @accept: string
- * Return: pointer to s
+ * _strpbrk - searches a string for any of a set of bytes.
+ * @s: first string.
+ * @accept: second string.
+ * Return: a pointer to the byte in s that matches one of the
+ * bytes in accept, or NULL if no such byte is found.
  */
 char *_strpbrk(char *s, char *accept)
 {
-	int k;
+	unsigned int i, j;
 
-	while (*s)
+	for (i = 0; *(s + i) != '\0'; i++)
 	{
-		for (k = 0; accept[k]; k++)
+		for (j = 0; *(accept + j) != '\0'; j++)
 		{
-			if (*s == accept[k])
-				return (s);
+			if (*(s + i) == *(accept + j))
+				return (s + i);
 		}
-		s++;
 	}
 	return ('\0');
 }
