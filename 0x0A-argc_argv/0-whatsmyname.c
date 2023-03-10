144c6689d4a2b6ddc1e0cd3a0bef8685f6889645
Hananiah Henry <hananiahjoseph66@gmail.com>
2023-03-10 03:45:44 -0800

    new

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,17 +1,19 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include "main.h"
 /**
  * main - entry point
  * @argc: argument count
  * @argv: argument value / arrays
  * Return: on Sucess 0
  */
-int main(argc, *argv[])
+int main(int argc, char *argv[])
 {
 	int i;
 
-	if (argc > 1)
-		for (i = 1; i < argc; i++)
+	if (argc >= 1)
+		for (i = 0; i < argc; i++)
 			printf("%s", argv[i]);
+	printf("\n");
 	return (0);
 }
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ * @argv: argument value
+ * @argc: argument count
+ * Return: success
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	int i;
+
+	for (i = 0; i < argc; i++);
+	printf("%d\n", i-1);
+	return (0);
+}
