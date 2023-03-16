ee4a904cd29f392fbbf56831ea84a2f73d7592b4
Hananiah Henry <hananiahjoseph66@gmail.com>
2023-03-15 18:55:55 -0700

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -18,6 +18,12 @@ char *str_concat(char *s1, char *s2)
 		s2 = "";
 	i = ci = 0;
 	while (s1[i] != '\0')
+		i++;
+	while (s2[ci] != '\0')
+		ci++;
+	conct = malloc(sizeof(char) * (i + ci + i));
+	i = ci = 0;
+	while (s1[i] != '\0')
 	{
 		conct[i] = s1[i];
 		i++;
