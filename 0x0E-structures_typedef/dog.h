ce9a87d50cc0e340a12c366dba04dcb616bac824
Hananiah Henry <122923660+Hananiahhenry1@users.noreply.github.com>
2023-03-21 04:37:55 +0100

    Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -11,28 +11,15 @@ struct my_dog
  * dog_t - typedef for struct dog
  */
 
-        /**
+/**
+* dog_t - typedef for struct dog
+*/
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
 
-         * dog_t - typedef for struct dog
-
-         */
-
-        typedef struct dog dog_t;
-
-
-
-        void init_dog(struct dog *d, char *name, float age, char *owner);
-
-        void print_dog(struct dog *d);
-
-        dog_t *new_dog(char *name, float age, char *owner);
-
-        void free_dog(dog_t *d);
-
-        char *_strcpy(char *dest, char *src);
-
-        int _strlen(char *s);
-
-
-
-        #endif
+#endif
