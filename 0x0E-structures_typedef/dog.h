f03cc3d23c93a718e6136472fd34c08c5bc94634
Hananiah Henry <hananiahjoseph66@gmail.com>
2023-03-21 11:37:33 -0700

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,20 +1,22 @@
 #ifndef DOG_H
 #define DOG_H
-
-typedef struct dog
+/**
+ * struct dog - struct
+ * @name: name of dog
+ * @age: age argument
+ * @owner: owner
+ */
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}my_dog;
-/**
- * dog_t - typedef for struct dog
- */
-
+};
 /**
 * dog_t - typedef for struct dog
 */
 typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
