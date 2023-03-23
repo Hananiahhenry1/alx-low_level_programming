3d77eceb86035dc68a43135a63e5f9c7596bfb41
Hananiah Henry <hananiahjoseph66@gmail.com>
2023-03-22 20:17:23 -0700

    Whats my name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 /**
  * print_name - function that prints a name
+ * @name: argument passed
+ * @f: pointer to function
  * Return: Nothing
  */
 void print_name(char *name, void (*f)(char *))
