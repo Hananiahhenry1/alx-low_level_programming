c939d8c5aa7741b9661edf5c2e3af6307047449a
Hananiah Henry <hananiahjoseph66@gmail.com>
2023-03-22 21:25:10 -0700

    int index

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+/**
+ * int_index - checks an array
+ * @array: arrays
+ * @size: size of array
+ * @cmp: function pointer
+ * Return: On success -1 or No of int
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (cmp == NULL || array == NULL || size <= 0)
+		return (-1);
+	i = 0;
+	while (i < size)
+	{
+		cmp(array[i]);
+		i++;
+		return (i);
+	}
+	return (-1);
+}
