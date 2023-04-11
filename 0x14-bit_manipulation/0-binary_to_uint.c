05649cd87f73922f50b6ecd4754672ab8bc895fd
Hananiah Henry <hananiahjoseph66@gmail.com>
2023-04-11 14:43:20 -0700

    binaryToInt

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,7 +12,7 @@ unsigned int binary_to_uint(const char *b)
 
 	for (i = 0; b[i]; i++)
 	{
-		if (*b < '0' || *b > '1')
+		if (b[i] < '0' || b[i] > '1')
 			return (0);
 		result = 2 * result + (b[i] - '0');
 	}
