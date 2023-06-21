c590954a20877eb2168ffdd6c4f6cef35b75f928
hananiahhenry1 <hananiahhenry1@gmail.com>
2023-06-21 03:19:33 +0100

    dog struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * dog - new struct
+ * @char: name of the dog
+ * @int: age of the dog
+ * @char: owner of the dog
+ * Description - creating a new dog struct
+*/
+struct dog
+{
+    char *name;
+    int age;
+    char *owner;
+};
+
+#endif
