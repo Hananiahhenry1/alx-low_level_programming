ed0ba7821f88aebccc7d6b307d91ee99ff213db4
hananiahhenry <hananiahhenry1@gmail.com>
2023-06-20 20:22:41 -0700

    struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,4 +14,13 @@ struct dog
 	char *owner;
 };
 
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+/**
+ * dog_t - typedef struct dog
+ */
+typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
 #endif
