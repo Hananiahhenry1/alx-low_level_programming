fe37d1be09d46edaa5882b562e1af87fc902c613
Hananiah Henry <122923660+Hananiahhenry1@users.noreply.github.com>
2023-07-05 23:38:59 +0100

    Create 1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _strdup - duplicate to new memory space location
+ * @str: char
+ * Return: 0
+ */
+char *_strdup(char *str)
+{
+	char *news;
+	int i, r = 0;
+
+	if (str == NULL)
+		return (NULL);
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+
+	news = malloc(sizeof(char) * (i + 1));
+
+	if (news == NULL)
+		return (NULL);
+
+	for (r = 0; str[r]; r++)
+		news[r] = str[r];
+
+	return (news);
+}
