7debe746e4443ae5509b9ed744d3edec6a0a4927
hananiahhenry <hananiahhenry1@gmail.com>
2023-08-21 22:52:08 -0700

    new added

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-gcc -wall -wextra -werror -pendatic -c -fPIC *.c
+gcc -Wall -Wextra -Werror -pedantic -c -fPIC *.c
 gcc -shared -o liball.so *.o
 export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
